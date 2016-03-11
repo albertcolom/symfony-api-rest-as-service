9123d915cd329c5c4904f8f734b56b28264af0f5
Albert Colom <skolom@gmail.com>
2016-03-11 14:04:42 +0100

    Extract Anotation Entity to yml

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -3,47 +3,32 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use JMS\Serializer\Annotation as JMS;
 
 /**
  * Product
- *
- * @ORM\Table(name="product")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
  */
 class Product
 {
 
     /**
      * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
      * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255)
      */
     private $name;
 
     /**
      * @var string
-     *
-     * @ORM\Column(name="description", type="text", nullable=true)
      */
     private $description;
 
     /**
      * @var bool
-     *
-     * @ORM\Column(name="active", type="boolean")
      */
     private $active;
-
 
     /**
      * Get id
