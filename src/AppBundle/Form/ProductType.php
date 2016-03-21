87618b3b55c9e0605626f514608de2fda1f0246e
Albert Colom <skolom@gmail.com>
2016-03-21 18:18:41 +0100

    Add ProductType.php

---
+++ src/AppBundle/Form/ProductType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Product;
+
+class ProductType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('active')
+            ->add('category')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Product::class,
+            'allow_extra_fields' => true,
+            'csrf_protection' => false
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
